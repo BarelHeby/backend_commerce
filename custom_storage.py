b4e4487351da23c9aa670c369ecff5bf3f88786d
BarelHeby <barelheby03@gmail.com>
2023-05-13 19:12:56 +0300

    remove custome arg in save image

--- custom_storage.py
+++ custom_storage.py
@@ -7,9 +7,9 @@ import os
 class CustomImageStorage(FileSystemStorage):
     def get_available_name(self, name, **kwargs):
         # Define a custom filename for the uploaded file
-        original_name, extension = os.path.splitext(name)
-        new_name = original_name + '_custom' + extension
-        return super().get_available_name(new_name, **kwargs)
+        # original_name, extension = os.path.splitext(name)
+        # new_name = original_name + '_custom' + extension
+        return super().get_available_name(name, **kwargs)
 
     @property
     def location(self):
