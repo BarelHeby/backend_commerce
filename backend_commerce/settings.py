e3a524ecfb6572f42db2704514c0aa9a9d90dfab
BarelHeby <barelheby03@gmail.com>
2023-05-13 19:59:20 +0300

    add avoid cache

--- backend_commerce/settings.py
+++ backend_commerce/settings.py
@@ -134,3 +134,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "assets")
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# avoid pycache
+os.environ['PYTHONDONTWRITEBYTECODE'] = '1'
